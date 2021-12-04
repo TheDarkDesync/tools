e6ea25588c88add79a8d7eff5628540bbde5c8ba
TheDarkDesync <philipmk69@gmail.com>
2021-12-05 00:45:10 +0100

    spongeText done

---
+++ spongetext/spongeText.py
@@ -0,0 +1,26 @@
+# from tkinter import Tk
+import win32clipboard
+
+win32clipboard.OpenClipboard()
+try:
+    text = list(win32clipboard.GetClipboardData())
+except TypeError:
+    text = " "
+win32clipboard.CloseClipboard()
+
+# spongeText = []
+idx = 1
+for i in range(len(text)):
+    if text[i].isalpha():
+        if idx % 2:
+            text[i] = text[i].upper()
+        else:
+            text[i] = text[i].lower()
+        idx += 1
+
+print("".join(text))
+
+win32clipboard.OpenClipboard()
+win32clipboard.EmptyClipboard()
+win32clipboard.SetClipboardText("".join(text))
+win32clipboard.CloseClipboard()
