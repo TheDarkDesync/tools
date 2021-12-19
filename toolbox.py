d187170b31a126d7a56b4c4e9f2ed2fa8e249de3
TheDarkDesync <philipmk69@gmail.com>
2021-12-19 23:41:54 +0100

    [feat] help window

--- toolbox.py
+++ toolbox.py
@@ -1,4 +1,4 @@
-from tkinter import Button, Tk
+from tkinter import Button, Label, Tk
 from tkinter.constants import FALSE
 from pdftools import extractimgfrompdf, imgtopdf, pdfmerger, pdfsplitter
 from texttools import spongetext
@@ -42,6 +42,30 @@ def spongeTextFunc():
         spongetext.textToClipboard(spongedText)
     except:
         print("Error: spongeTextFunc")
+        
+def helpFunc():
+    help = Tk()
+    help.title("Help")
+    help.resizable(False, False)
+    help.configure(background="#0d1117")
+    Label(help, text="""
+        SpongeText
+        Formats the text from your clipboard in alternating upper and lowercase.
+
+        PDF-Merger
+        Merges the selected PDFs and saves the merged PDF to your desired location.
+
+        PDF-Splitter
+        Splits the selected PDF into the individual pages as PDFs and saves them to your desired location.
+
+        Image to PDF
+        Converts selected images to PDFs and saves them to your desired location.
+
+        Extract Images from PDF
+        Extracts all images from the selected PDF and saves them to your desired location.""", background="#0d1117", foreground="#c9d1d9", font="SauceCodePro 12", justify="left"
+    ).pack(side="top", fill="x", padx="2", pady="10")
+    Button(help, text="Close Help", command=lambda: help.destroy(), background="#da3633", activebackground="#8d2321", border=0, cursor="hand2").pack(side="left", padx="10", pady="10")
+    
     
 if __name__ == "__main__":
     root = Tk()
@@ -49,11 +73,12 @@ if __name__ == "__main__":
     root.resizable(False, False)
     root.configure(background="#0d1117")
     Button(text="TOOLBOX", font="SauceCodePro 20", background="#2ea043", activebackground="#2ea043", border=0, relief="sunken", command=lambda: open("https://bit.ly/32hnScq")).pack(side="top", fill="x", padx="10", pady="10")
-    Button(text="Image-Extractor from PDF", command=lambda: imageExtractorFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb").pack(side="top", fill="x", padx="10", pady="10")
-    Button(text="Image-To-PDF", command=lambda: imgToPdfFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb").pack(side="top", fill="x", padx="10", pady="10")
-    Button(text="PDF-Merger", command=lambda: pdfMergerFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb").pack(side="top", fill="x", padx="10", pady="10")
-    Button(text="PDF-Splitter", command=lambda: pdfSplitterFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb").pack(side="top", fill="x", padx="10", pady="10")
-    Button(text="Sponge-Text", command=lambda: spongeTextFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb").pack(side="top", fill="x", padx="10", pady="10")
-    Button(text="Quit Toolbox", command=lambda: root.quit(), background="#da3633", activebackground="#8d2321", border=0).pack(side="top", fill="x", padx="10", pady="10")
+    Button(text="Image-Extractor from PDF", command=lambda: imageExtractorFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb", cursor="hand2").pack(side="top", fill="x", padx="10", pady="10")
+    Button(text="Image-To-PDF", command=lambda: imgToPdfFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb", cursor="hand2").pack(side="top", fill="x", padx="10", pady="10")
+    Button(text="PDF-Merger", command=lambda: pdfMergerFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb", cursor="hand2").pack(side="top", fill="x", padx="10", pady="10")
+    Button(text="PDF-Splitter", command=lambda: pdfSplitterFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb", cursor="hand2").pack(side="top", fill="x", padx="10", pady="10")
+    Button(text="Sponge-Text", command=lambda: spongeTextFunc(), border=0, activebackground="#13233a", activeforeground="#d1e6ff", background="#afb5bb", cursor="hand2").pack(side="top", fill="x", padx="10", pady="10")
+    Button(text="Help", command=lambda: helpFunc(), border=0, activebackground="#7f8387", activeforeground="#eef3ff", background="#eef3ff", cursor="hand2").pack(side="right", padx="10", pady="10")
+    Button(text="Quit Toolbox", command=lambda: root.quit(), background="#da3633", activebackground="#8d2321", border=0, cursor="hand2").pack(side="left", padx="10", pady="10")
     root.wm_geometry("350x325")
     root.mainloop()
