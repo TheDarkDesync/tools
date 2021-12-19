455c03e67f19eb6424a5b5f22fbf83565ee5f7cb
TheDarkDesync <philipmk69@gmail.com>
2021-12-19 19:00:52 +0100

    [refactor] create functions

--- standalonetools/pdfmerger.py
+++ standalonetools/pdfmerger.py
@@ -4,21 +4,35 @@ from tkinter.filedialog import askdirectory, askopenfilenames
 import sys
 
 # Select PDFs from files & check 
-Tk().withdraw()
-pdfs = askopenfilenames(filetypes=[("PDFs", "*.pdf")])
+def selectFiles():
+    Tk().withdraw()
+    pdfFilenames = askopenfilenames(filetypes=[("PDFs", "*.pdf")])
 
-# Exit if only one PDF is selected
-if len(pdfs) < 2:
-    sys.exit("You selected only one PDF... Can not merge")
+    # Exit if only one PDF is selected
+    if len(pdfFilenames) < 2:
+        sys.exit("You selected only one PDF... Can not merge")
+        
+    return pdfFilenames
+
 
 # Merge PDFs into a single PDF
-mergedPdf = Pdf.new()
-mergedName = ""
-for pdf in pdfs:
-    src = Pdf.open(pdf)
-    mergedName = mergedName + pdf.split("/")[-1].split(".")[0]
-    mergedPdf.pages.extend(src.pages)
+def mergePdfs(pdfFilenames):
+    mergedPdf = Pdf.new()
+    mergedName = ""
+    for pdfFile in pdfFilenames:
+        src = Pdf.open(pdfFile)
+        mergedName = mergedName + pdfFile.split("/")[-1].split(".")[0]
+        mergedPdf.pages.extend(src.pages)
+    
+    return mergedPdf, mergedName
 
 # Save PDF to selected directory
-saveLoc = askdirectory()
-mergedPdf.save("{dir}/{name}.pdf".format(dir=saveLoc, name = mergedName))
+def savePdf(pdf, name): 
+    saveLoc = askdirectory()
+    pdf.save("{dir}/{name}.pdf".format(dir=saveLoc, name = name))
+
+
+if __name__ == "__main__":
+    pdfs = selectFiles()
+    mergedPdf, name = mergePdfs(pdfs)
+    savePdf(mergedPdf, name)
