88f9d0ebdfd8e29d614ed4fb48f88bc831ae3db1
TheDarkDesync <philipmk69@gmail.com>
2021-12-19 19:09:39 +0100

    [refactor] code to functions

--- standalonetools/spongetext.py
+++ standalonetools/spongetext.py
@@ -1,22 +1,36 @@
 import win32clipboard
 
-win32clipboard.OpenClipboard()
-try:
-    text = list(win32clipboard.GetClipboardData())
-except TypeError:
-    text = " "
-win32clipboard.CloseClipboard()
+# Gets the text from the clipboard 
+def textFromClipboard():
+    win32clipboard.OpenClipboard()
+    try:
+        text = list(win32clipboard.GetClipboardData())
+    except TypeError:
+        text = " "
+    win32clipboard.CloseClipboard()
+    return text
 
-idx = 1
-for i in range(len(text)):
-    if text[i].isalpha():
-        if idx % 2:
-            text[i] = text[i].upper()
-        else:
-            text[i] = text[i].lower()
-        idx += 1
+# Sponges the text
+def spongeTheText(text):
+    idx = 1
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if idx % 2:
+                text[i] = text[i].upper()
+            else:
+                text[i] = text[i].lower()
+            idx += 1
+    
+    return "".join(text)
 
-win32clipboard.OpenClipboard()
-win32clipboard.EmptyClipboard()
-win32clipboard.SetClipboardText("".join(text))
-win32clipboard.CloseClipboard()
+# Puts the text to the clipboard
+def textToClipboard(text):
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardText(text)
+    win32clipboard.CloseClipboard()
+    
+if __name__ == "__main__":
+    text = textFromClipboard()
+    spongedText = spongeTheText(text)
+    textToClipboard(spongedText)
